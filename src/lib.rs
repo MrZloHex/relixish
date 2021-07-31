19c7aa68b1108cb39862f0da7e1ef421b6df707e
MrZloHex <zlo.alex.it@gmail.com>
2021-07-31 13:20:49 +0300

    DATA

--- src/lib.rs
+++ src/lib.rs
@@ -6,5 +6,6 @@ use analyser::Analyser;
 pub fn analys(string: String) -> HashMap<String, String> {
     let mut analyser = Analyser::new();
     analyser.add_string(string);
-    analyser.analys()
+    let output = analyser.analys();
+    output
 }
